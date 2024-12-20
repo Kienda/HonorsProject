296238a60026ed39b91572572da2ac4c5695143a
AbdoulayeAbdoulaye Alhassane Diallo <alhassaneabdoulaye@gmail.com>
2024-12-20 01:58:28 -0500

    Resolved merge conflict

--- studentSuccessUpdate/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_studentSuccessUpdate_FILE E:/College/Fall24/HonorProjects/HonorsProject/studentSuccessUpdate/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/studentSuccessUpdate.exe)
-set(__QT_DEPLOY_TARGET_studentSuccessUpdate_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_studentSuccessUpdate_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
